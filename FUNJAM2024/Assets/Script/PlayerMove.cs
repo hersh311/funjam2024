0797ec12d8e9fe1d896c4d24e9706acbced11e51
N.D. Bhavana <ndbhavanastarts@gmail.com>
2024-02-21 23:02:05 -0800

    update_playermov

--- FUNJAM2024/Assets/Script/PlayerMove.cs
+++ FUNJAM2024/Assets/Script/PlayerMove.cs
@@ -8,11 +8,12 @@ public class PlayerMove : MonoBehaviour
     private float inputV = 0;
     public float speed = 10;
     public Rigidbody2D rb;
-
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,7 +21,14 @@ public class PlayerMove : MonoBehaviour
     {
         inputH = Input.GetAxis("Horizontal");
         inputV = Input.GetAxis("Vertical");
-
+        if(inputH==0 && inputV == 0)
+        {
+            animator.SetBool("is_moving",false);
+        }
+        else
+        {
+            animator.SetBool("is_moving", true);
+        }
         rb.velocity = new Vector2(speed * inputH, speed * inputV);
 
     }
